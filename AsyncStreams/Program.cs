6fc84a432938d9b847b64b8ce18ceedb4f208148
Fernando Ferreira <fthomm@msn.com>
2019-07-08 20:45:51 -0300

    Inclusão dos projetos.

---
+++ AsyncStreams/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsyncStreams
+{
+    class Program
+    {
+        //Forma tradicional
+        //  private async static IEnumerable<int> GetHeartBeat()
+        //         {
+        //             await Task.Delay(3000);
+        //             Console.WriteLine($"Last check: {DateTime.Now}");
+        //              yield return new Random().Next(40, 250);
+        //         }
+
+        //Utiliazndo IAsyncEnumerable
+        private async static IAsyncEnumerable<int> GetHeartBeatAsync()
+        {
+            await Task.Delay(3000);
+            Console.WriteLine($"Last check: {DateTime.Now}");
+            yield return new Random().Next(40, 250);
+        }
+
+        private async static void ShowHeartBeatAsync()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+
+                await foreach (var item in GetHeartBeatAsync())
+                {
+                    Console.WriteLine($"Current heart beat: {item}\n");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ShowHeartBeatAsync();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SwitchExpressions/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        enum Operacao { Addition, Subtraction, Division, Multiplication }
+
+        //Forma tradicional
+        //static int SolveOperation(int a, int b, Operacao operation)
+        //{
+        //    var total = 0;
+
+        //    switch (operation)
+        //    {
+        //        case Operacao.Addition:
+        //            total = a + b;
+        //            break;
+
+        //        case Operacao.Subtraction:
+        //            total = Math.Abs(a - b);
+        //            break;
+
+        //        case Operacao.Division:
+        //            total = a / b;
+        //            break;
+
+        //        case Operacao.Multiplication:
+        //            total = a * b;
+        //            break;
+
+        //        default:
+        //            total = 0;
+        //            break;
+        //    }
+        //    return total;
+        //}
+
+
+        //Switch expressions
+        static int SolveOperation(int a, int b, Operacao operation)
+        {
+
+            var total = operation switch
+            {
+                Operacao.Addition => a + b,
+                Operacao.Subtraction => Math.Abs(a - b),
+                Operacao.Division => ((Func<int>)(() =>
+                {
+                    try
+                    {
+                        return a / b;
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }))(),
+                Operacao.Multiplication => a * b,
+                _ => 0
+            };
+
+            return total;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(SolveOperation(5, 7, Operacao.Multiplication));
+        }
+    }
+}
